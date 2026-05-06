b7ae28a617bb13ac68e76ad7f13289713d0b9e19
pallavireddymerugu <merugupallavi45@gmail.com>
2026-05-06 13:31:55 +0530

    Update queries.sql

--- Week-1/Day-1/queries.sql
+++ Week-1/Day-1/queries.sql
@@ -1,3 +1,16 @@
 -- Question 1
 --Displyaing All columns
 SELECT*FROM Employee;
+--Q2
+--Selecting only name and salary
+SELECT name,salary FROM Employee;
+--Q3
+--older than 50
+SELECT*FROM Employee where age>30
+--Q4
+--Names od departments
+SELECT name FROM Department;
+--Q5
+--employees in IT
+SELECT*FROM Employee where department_id=1;
+--
